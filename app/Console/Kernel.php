d6517c87989e19374fc349d206716ee1f070efe6
mudev939 <gharablipro19933@gmail.com>
2024-02-05 10:43:46 +0300

    first commit

---
+++ app/Console/Kernel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console;
+
+use App\Jobs\SendMail;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->hourly();
+
+        // $schedule->call(function () {
+        //     Log::info('Welcome to my project');
+        // })->everySecond();
+
+        $schedule->job(new SendMail)->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
---
+++ app/Jobs/SendMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::raw("Welcome to my project",function($message){
+
+            $message->to("gharablipro19933@gmail.com")
+            ->subject('Welcome');
+
+        });
+    }
+}
